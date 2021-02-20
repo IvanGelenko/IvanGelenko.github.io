393c3bb6ba90e1b82aea73b773d98987a9a1849b
IvanGelenko <48840940+IvanGelenko@users.noreply.github.com>
2021-02-20 17:51:57 +0300

    добавлен бурген

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('burger').onclick = function () {
+
+    var MobileMenu = document.getElementById('MobileMenu');
+    var MainBody = document.getElementById('MainBody');
+    var MobileMenuUl = document.getElementById('MobileMenuUl');
+
+
+    if (MobileMenu.className == 'NonActive') {
+        MobileMenu.className = 'Active';
+        MainBody.className='overflow';
+        MobileMenuUl.className='MobileMenuUlActive';
+    } else {
+        MobileMenu.className = 'NonActive';
+        MainBody.className='NonMenu';
+        MobileMenuUl.className='NonActive';
+    }
+
+       
+}
